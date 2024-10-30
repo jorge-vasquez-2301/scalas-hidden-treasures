a9d4768110ccafe5aa7ba6b271ebd2bb29c23f90
Jorge Vasquez <jorge.vasquez0123@proton.me>
2024-10-30 14:36:01 -0400

    Initial slides

---
+++ global-bottom.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="absolute bottom-0 left-12 right-0 p-2 z-50">
+        <LightOrDark>
+            <template #dark>
+                <img src="/zivergeLogo.png" class="h-4" />
+            </template>
+            <template #light>
+                <img src="/zivergeLogo.png" class="h-4" />
+            </template>
+        </LightOrDark>
+    </footer>
+
+    <footer class="absolute bottom-0 right-12 p-2 z-50">
+        <SlideCurrentNo /> /
+        <SlidesTotal />
+    </footer>
+</template>
